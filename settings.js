015368318369c953f1ba5fd23c46938d587e242a
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-08 02:27:58 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;SYVlgAYa#mAFkbE6kQuIgG_0LYXQc4RZbLihH0ipH6I3u3ORzE9w", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "27767494368", // Put Owner Number Here 
+  PREFIX: "+" // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
 // CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
